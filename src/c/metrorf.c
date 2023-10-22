2b0d55bc153396595364f5277c4fd7af5fa7c639
t0ster30 <stegosha@gmail.com>
2023-10-22 21:48:31 +0300

    svg changed, pdc made

--- src/c/metrorf.c
+++ src/c/metrorf.c
@@ -2,17 +2,28 @@
 
 static Window *s_window;
 static TextLayer *s_text_layer;
+static GDrawCommandImage *s_command_image;
+
+GColor metro_red = GColorFromRGB(220, 50, 32);
+GColor tram_green = GColorFromRGB(25, 157, 116);
+GColor bus_yellow = GColorFromRGB(227, 158, 35);
 
 static void prv_select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(s_text_layer, "Select");
+  text_layer_set_text(s_text_layer, "2");
+  window_set_background_color(s_window, metro_red);
+  text_layer_set_background_color(s_text_layer, metro_red);
 }
 
 static void prv_up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(s_text_layer, "Up");
+  text_layer_set_text(s_text_layer, "1");
+  window_set_background_color(s_window, tram_green);
+  text_layer_set_background_color(s_text_layer, tram_green);
 }
 
 static void prv_down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(s_text_layer, "Down");
+  text_layer_set_text(s_text_layer, "3");
+  window_set_background_color(s_window, bus_yellow);
+  text_layer_set_background_color(s_text_layer, bus_yellow);
 }
 
 static void prv_click_config_provider(void *context) {
@@ -25,8 +36,9 @@ static void prv_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  s_text_layer = text_layer_create(GRect(0, 72, bounds.size.w, 20));
-  text_layer_set_text(s_text_layer, "Press a button");
+  s_text_layer = text_layer_create(GRect(0, 66, bounds.size.w, bounds.size.h));
+  text_layer_set_font(s_text_layer, fonts_get_system_font(FONT_KEY_LECO_42_NUMBERS));
+  text_layer_set_text(s_text_layer, "0");
   text_layer_set_text_alignment(s_text_layer, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(s_text_layer));
 }
@@ -37,6 +49,7 @@ static void prv_window_unload(Window *window) {
 
 static void prv_init(void) {
   s_window = window_create();
+  window_set_background_color(s_window, GColorWhite);
   window_set_click_config_provider(s_window, prv_click_config_provider);
   window_set_window_handlers(s_window, (WindowHandlers) {
     .load = prv_window_load,
